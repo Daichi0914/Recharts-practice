d660fb2d0d7c0316f7c528580df8f647832e1c10
Daichi0914 <goludrozya44@gmail.com>
2020-07-10 17:40:21 +0900

    Updates

---
+++ precache-manifest.4173f29234141b6ee38f184999d9c76d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b64b1d1603854d134b76be18343ecf",
+    "url": "/Recharts-practice/index.html"
+  },
+  {
+    "revision": "d7c25c2908cfa597ebb8",
+    "url": "/Recharts-practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fafb943ee6e66bc057bf",
+    "url": "/Recharts-practice/static/js/2.aa6f28b8.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/Recharts-practice/static/js/2.aa6f28b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c25c2908cfa597ebb8",
+    "url": "/Recharts-practice/static/js/main.6ec94d31.chunk.js"
+  },
+  {
+    "revision": "0b04961e335129e95481",
+    "url": "/Recharts-practice/static/js/runtime-main.832388bf.js"
+  }
+]);
